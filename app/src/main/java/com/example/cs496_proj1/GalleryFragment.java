ec2ebbfa6d2f5c74699ce880a7238c4ca606771a
woohyein <whyein@kaist.ac.kr>
2020-12-31 01:05:57 +0900

    1230_Gallery&Permission

--- app/src/main/java/com/example/cs496_proj1/GalleryFragment.java
+++ app/src/main/java/com/example/cs496_proj1/GalleryFragment.java
@@ -1,14 +1,42 @@
 package com.example.cs496_proj1;
 
+import android.Manifest;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import org.xmlpull.v1.XmlPullParser;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
 
 public class GalleryFragment extends Fragment {
+    private GridView gridview;
 
     public GalleryFragment() {
     }
@@ -21,11 +49,97 @@ public class GalleryFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (ContextCompat.checkSelfPermission(Objects.requireNonNull(getActivity()),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // request the permission
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+        }
+
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_gallery, container, false);
     }
+
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    public static class ImageUnit{
+        Long id;
+        Uri imageUri;
+
+        public ImageUnit(Long id, Uri imageUri) {
+        }
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    public List<ImageUnit> LoadImages(){
+        List<ImageUnit> FileList = new ArrayList<>();
+        String[] projection = {
+                MediaStore.Images.Media._ID,
+                MediaStore.Images.Media.DISPLAY_NAME,
+                MediaStore.Images.Media.DATE_TAKEN
+        };
+
+        Uri uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        Cursor cursor = Objects.requireNonNull(getActivity()).getContentResolver().query(uri, projection, null, null, null);
+
+        while(cursor.moveToNext()){
+            Long id = cursor.getLong(cursor.getColumnIndex(MediaStore.MediaColumns._ID));
+            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+            FileList.add(new ImageUnit(id, imageUri));
+        }
+        cursor.close();
+        return FileList;
+    }
+
+    static class GalleryAdapter extends BaseAdapter {
+        private final List<ImageUnit> FileList;
+        private final Context context;
+        LayoutInflater inf;
+
+        public GalleryAdapter(List<ImageUnit> FileList, Context context){
+            this.FileList = FileList;
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return FileList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return FileList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return FileList.get(position).id;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            if (convertView == null) convertView = inf.inflate((XmlPullParser) context, null);
+            ImageView iv = (ImageView) convertView.findViewById(R.id.tabs);      // tabs가 맞는지 모름
+            iv.setImageResource(position);// ?
+            Glide.with(context)
+                    .load(FileList.get(position).imageUri)
+                    .centerCrop()
+                    .into(iv);
+
+
+            return convertView;
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/cs496_proj1/MainActivity.java
+++ app/src/main/java/com/example/cs496_proj1/MainActivity.java
@@ -2,11 +2,20 @@ package com.example.cs496_proj1;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
@@ -14,6 +23,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+    String[] REQUIRED_PERMISSIONS  = {Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
     private ViewPager2 viewPager;
     TabPagerAdapter pagerAdapter;
@@ -62,9 +75,82 @@ public class MainActivity extends AppCompatActivity {
             public void onTabReselected(TabLayout.Tab tab) {
             }
         });
+            GetPermission();
+            onRequestPermissionsResult(PERMISSIONS_REQUEST_CODE, REQUIRED_PERMISSIONS, );
+        }
 
+        public void GetPermission() {
+            //int status = ActivityCompat.checkSelfPermission(Context context, String permission);
+            CoordinatorLayout mLayout;
+            int Permission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+            int writeExternalStoragePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            if (!(Permission == PackageManager.PERMISSION_GRANTED
+                    && writeExternalStoragePermission == PackageManager.PERMISSION_GRANTED)) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])
+                        || ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[1])) {
+                    Snackbar.make(mLayout, "이 앱을 실행하려면 카메라와 외부 저장소 접근 권한이 필요합니다.",
+                            Snackbar.LENGTH_INDEFINITE).setAction("확인", new View.OnClickListener() {
+
+                        @Override
+                        public void onClick(View view) {
+                            // 3-3. 사용자게에 퍼미션 요청을 합니다. 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                            ActivityCompat.requestPermissions(MainActivity.this, REQUIRED_PERMISSIONS,
+                                    PERMISSIONS_REQUEST_CODE);
+                        }
+                    }).show();
+                } else {
+                    // 4-1. 사용자가 퍼미션 거부를 한 적이 없는 경우에는 퍼미션 요청을 바로 합니다.
+                    // 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                    ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS,
+                            PERMISSIONS_REQUEST_CODE);
+                }
+            }
+        }
+
+            @Override
+            public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grandResults) {
+                CoordinatorLayout mLayout;
+                if ( requestCode == PERMISSIONS_REQUEST_CODE && grandResults.length == REQUIRED_PERMISSIONS.length) {
+
+                    // 요청 코드가 PERMISSIONS_REQUEST_CODE 이고, 요청한 퍼미션 개수만큼 수신되었다면
+                    boolean check_result = true;
+
+                    // 모든 퍼미션을 허용했는지 체크합니다.
+                    for (int result : grandResults) {
+                        if (result != PackageManager.PERMISSION_GRANTED) {
+                            check_result = false;
+                            break;
+                        }
+                    }
+
+                    if ( !check_result){
+                        // 거부한 퍼미션이 있다면 앱을 사용할 수 없는 이유를 설명해주고 앱을 종료합니다.2 가지 경우가 있습니다.
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])
+                                || ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[1])) {
+
+                            // 사용자가 거부만 선택한 경우에는 앱을 다시 실행하여 허용을 선택하면 앱을 사용할 수 있습니다.
+                            Snackbar.make(mLayout, "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요. ",
+                                    Snackbar.LENGTH_INDEFINITE).setAction("확인", new View.OnClickListener() {
+
+                                @Override
+                                public void onClick(View view) {
+                                    finish();
+                                }
+                            }).show();
+                        }else {
+                            // “다시 묻지 않음”을 사용자가 체크하고 거부를 선택한 경우에는 설정(앱 정보)에서 퍼미션을 허용해야 앱을 사용할 수 있습니다.
+                            Snackbar.make(mLayout, "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ",
+                                    Snackbar.LENGTH_INDEFINITE).setAction("확인", new View.OnClickListener() {
+
+                                @Override
+                                public void onClick(View view) {
+                                    finish();
+                                }
+                            }).show();
+                        }
+                    }
     }
-
+}
 }
 
 
